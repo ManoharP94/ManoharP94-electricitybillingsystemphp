d37a91a68397dc9298a5fc7bcbdb4e1bcde4b5cb
Manohar Patil <58136430+ManoharP94@users.noreply.github.com>
2021-10-04 23:27:26 +0530

    Add files via upload

---
+++ electricitybillingsystemphp/contact.php
@@ -0,0 +1,92 @@
+
+<html lang="en-US">
+    <head>
+        
+        <meta charset="UTF-8">
+	<meta http-equiv="X-UA-compatible" content = "IE-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        
+        <?php include "header.php"; ?> 
+        <style>
+            .mydiv{
+            height:300px;
+             overflow-y: scroll;
+                    }
+        </style>
+    </head>
+    <body style = "background-color: white">
+       
+       <?php include "navigationbar1.php"; ?>
+
+	   <div class="container" style = "margin-top : 100px">
+	   <h2 class=text-center style = "color : red">Maharashtra State Electricity Board,</h2>
+	   <div class=row>
+	   
+   <div class="col-md-3 d-flex">
+          	<div class="align-self-stretch box p-4 py-md-5 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Address</h3>
+	            <p>Dondaicha, Dhule - 425408, At Post Dondaicha Near Shivaji Statue Behind Girls High School</p>
+	          </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="align-self-stretch box p-4 py-md-5 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-phone2"></span>
+          		</div>
+          		<h3 class="mb-4">Contact Number</h3>
+	            <p><a href="#">02566-20203030 To 20204065</a></p>
+	          </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="align-self-stretch box p-4 py-md-5 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-paper-plane"></span>
+          		</div>
+          		<h3 class="mb-4">Email Address</h3>
+	            <p><a href="mailto:contact@mseb.co.in">contact@mseb.co.in</a></p>
+	          </div>
+          </div>
+          <div class="col-md-3 d-flex">
+          	<div class="align-self-stretch box p-4 py-md-5 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-globe"></span>
+          		</div>
+          		<h3 class="mb-4">Fax</h3>
+	            <p>02566-2589821</p>
+	          </div>
+          </div>
+		  
+		  </div>
+		  </div>
+          
+		  
+
+ 
+ 
+		  
+		  
+		  
+		  
+		  
+        </body>
+		</head>
+		</html>
+      
+   
+Madhya Maharashtra State Electricity Board,
+Address – Dondaicha, Dhule - 425408, At Post Dondaicha Near Shivaji Statue Behind Girls High School
+
+Contact – 02566-20203030 To 20204065
+
+Fax – 02566-2589821
+
+Email – contact@mseb.co.in
